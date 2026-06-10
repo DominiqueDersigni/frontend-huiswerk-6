dd8c147b387248d1f1439b18bf8f52e9e67ec8b3
dominiquedersigni <dominiquedersigni@gmail.com>
2026-06-10 20:57:39 +0200

    loops-arrays-objects.js-finished

--- loops-arrays-objects.js
+++ loops-arrays-objects.js
@@ -23,6 +23,11 @@ const salaryDetails = [
 	{name: 'Otis', salary: 2800}
 ];
 
+
+for (let i = 0; i < salaryDetails.length; i++) {
+	console.log('Beste ' + salaryDetails[i].name + ', je salaris van ' + salaryDetails[i].salary + ',- is zojuist overgemaakt. Val me nu alsjeblieft niet meer lastig.');
+}
+
 // ==========================================
 // Opdracht 2. Wanneer iemand een verlof-aanvraag doet, moet Bob eerst uit zijn hoofd uitrekenen of deze medewerker nog
 // voldoende vrije dagen over heeft voor de aanvraag. En dat gaat nog wel eens mis... Zorg ervoor dat het aantal vakantiedagen
@@ -44,6 +49,9 @@ const vacationDays = [
 	{name: 'Otis', totalVacationDays: 25, usedVacationDays: 7},
 ];
 
+for (let i = 0; i < vacationDays.length; i++) {
+	console.log(vacationDays[i].name + ' heeft nog ' + (vacationDays[i].totalVacationDays - vacationDays[i].usedVacationDays) + ' vakantiedagen over.');
+}
 
 // ==========================================
 // Opdracht 3. Bob heeft een lijst van medewerkers en de opleidingen die ze volgen. Hij wil weten wie er een opleiding volgen
@@ -62,6 +70,12 @@ const employeesInTraining = [
 	{name: 'Nina', training: 'Teamworktraining', month: 'Maart'},
 	{name: 'Otis', training: 'Teamworktraining', month: 'Februari'},
 ];
+
+for (let i = 0; i < employeesInTraining.length; i++) {
+	if (employeesInTraining[i].month === 'Januari') {
+		console.log(employeesInTraining[i].name + ': ' + employeesInTraining[i].training);
+	}
+}
 
 // ==========================================
 // Opdracht 4a. Medewerkers worden ieder jaar beoordeelt op hun functioneren. Het is aan Bob om de scores om te zetten
@@ -89,6 +103,23 @@ const scores = [
 	{name: 'Otis', score: 100, salaryIncrease: null},
 ];
 
+for (let i = 0; i < scores.length; i++) {
+	if (scores[i].score < 60) {
+		console.log('0%');
+	}
+	else if (scores[i].score >= 60 && scores[i].score <= 69) {
+		console.log('2%');
+	}
+	else if (scores[i].score >= 70 && scores[i].score <= 89) {
+		console.log('3%');
+	}
+	else if (scores[i].score >= 90 && scores[i].score < 100) {
+		console.log('4%');
+	}
+	else {console.log('6%');
+	}
+}
+
 // ==========================================
 // Opdracht 4b. Breid je script uit door het percentage op te slaan in de 'salaryIncrease'-property van ieder object in de array.
 
@@ -103,6 +134,29 @@ const scores = [
 // 	{name: 'Otis', score: 100, salaryIncrease: '6%'},
 // ];
 // ==========================================
+
+for (let i = 0; i < scores.length; i++) {
+	if (scores[i].score < 60) {
+		scores[i].salaryIncrease = '0%';
+		console.log('0%');
+	}
+	else if (scores[i].score >= 60 && scores[i].score <= 69) {
+		scores[i].salaryIncrease = '2%';
+		console.log('2%');
+	}
+	else if (scores[i].score >= 70 && scores[i].score <= 89) {
+		scores[i].salaryIncrease = '3%';
+		console.log('3%');
+	}
+	else if (scores[i].score >= 90 && scores[i].score < 100) {
+		scores[i].salaryIncrease = '4%';
+		console.log('4%');
+	}
+	else {scores[i].salaryIncrease = '6%';
+		console.log('6%');
+	}
+}
+console.log(scores)
 
 
 
